c755ac0f5597a52549d7fdc8c14c3a5bbb72943e
HansolWorld <hs9269@gmail.com>
2023-09-15 11:13:49 +0900

    View: 애플 로그인 뷰 변경

--- meokQ-Boss-ios/View/Login/AppleLoginButtonView.swift
+++ meokQ-Boss-ios/View/Login/AppleLoginButtonView.swift
@@ -20,39 +20,58 @@ struct AppleLoginButtonView: View {
     @State var fullName = ""
     
     var body: some View {
-        SignInWithAppleButton { (request) in
-            
-            appleLoginViewModel.currentNonce = randomNonceString()
-            request.requestedScopes = [.email, .fullName]
-            request.nonce = sha256(appleLoginViewModel.currentNonce)
-        } onCompletion: { (result) in
-            
-            switch result {
-            case .success(let user):
-                Task {
-                    
-                    guard let credential = user.credential as? ASAuthorizationAppleIDCredential else {
-                        Log("error with firebase")
-                        return
-                    }
-                    if let fullName = credential.fullName {
-                        self.fullName = "\(fullName.familyName ?? "")" + "\(fullName.givenName ?? "")"
-                        Log(self.fullName)
-                    }
-                    if let email = credential.email {
-                        Log(email)
-                    }
-                    Log(credential.user)
-                    await appleLoginViewModel.authenticate(credential: credential)
-                    self.uid = appleLoginViewModel.firebaseuid
-                    await userViewModel.addNewUser(uid: self.uid, displayName: self.fullName)
-                    isLogin = true
-                }
-            case .failure(let error):
-                Log(error.localizedDescription)
+        ZStack {
+            Color.black
+            HStack {
+                Image(LoginButton.apple.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 18)
+                Spacer()
+                Text(LoginButton.apple.labelText)
+                    .foregroundColor(LoginButton.apple.accentColor)
+                //                        .font(.system(size: 14).weight(.medium))
+                    .font(.custom("SFProDisplay-Medium", size: 14))
+                Spacer()
             }
-            
+            .padding(.horizontal, 12)
         }
         .frame(width: 250, height: 50)
+        .overlay {
+            SignInWithAppleButton { (request) in
+                appleLoginViewModel.currentNonce = randomNonceString()
+                request.requestedScopes = [.email, .fullName]
+                request.nonce = sha256(appleLoginViewModel.currentNonce)
+            } onCompletion: { (result) in
+                
+                switch result {
+                case .success(let user):
+                    Task {
+                        
+                        guard let credential = user.credential as? ASAuthorizationAppleIDCredential else {
+                            Log("error with firebase")
+                            return
+                        }
+                        if let fullName = credential.fullName {
+                            self.fullName = "\(fullName.familyName ?? "")" + "\(fullName.givenName ?? "")"
+                            Log(self.fullName)
+                        }
+                        if let email = credential.email {
+                            Log(email)
+                        }
+                        Log(credential.user)
+                        await appleLoginViewModel.authenticate(credential: credential)
+                        self.uid = appleLoginViewModel.firebaseuid
+                        await userViewModel.addNewUser(uid: self.uid, displayName: self.fullName)
+                        isLogin = true
+                    }
+                case .failure(let error):
+                    Log(error.localizedDescription)
+                }
+                
+            }
+            .blendMode(.overlay)
+        }
+        .clipped()
     }
 }
